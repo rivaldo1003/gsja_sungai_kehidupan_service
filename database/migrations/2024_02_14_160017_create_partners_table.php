e8b3dead1d2cfc8ba7d2c7140180426ea9069778
Rivaldo Siregardwdedwed <rvldsiregar@gmail.com>
2024-02-14 18:19:02 +0700

    Menambahkan tabel partners pada database

--- database/migrations/2024_02_14_160017_create_partners_table.php
+++ database/migrations/2024_02_14_160017_create_partners_table.php
@@ -4,24 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreatePartnersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('partners', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user1_id');
+            $table->unsignedBigInteger('user2_id');
             $table->timestamps();
+
+            $table->foreign('user1_id')->references('id')->on('users');
+            $table->foreign('user2_id')->references('id')->on('users');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('partners');
     }
-};
+}
